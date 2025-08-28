6f94ecba549a601ebe10990114c96e6deac16806
mninosua <mninosua@emeal.nttdata.com>
2025-08-28 11:23:36 -0500

    library-schema:schema:V0.0.1

---
+++ projects/schema/src/lib/schema.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sh-schema',
+  imports: [],
+  template: `
+    <p>
+      schema works!
+    </p>
+  `,
+  styles: ``
+})
+export class SchemaComponent {
+
+}
---
+++ projects/schema/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of schema
+ */
+
+export * from './lib/schema.service';
+export * from './lib/schema.component';
